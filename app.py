0e76ce6db41d8fcd332763e22f29af0e39ce455c
MinGyeong Kim <mink0502k@gmail.com>
2024-08-02 01:30:48 +0900

    1

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+app.config['DEBUG'] ='True'
 
 
 @app.route("/")
 def hello():
     return render_template("main.html", title="Jinja test")
+
+if __name__ == '__main__':
+    app.run(debug=True)
