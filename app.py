d99cf97efe1d753e52ab77d47fae3788bcfbcd38
민석 <minseok050419@gmail.com>
2024-08-02 21:51:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] ='True'
 
 @app.route("/")
 def hello():
-    return render_templates("main.html", title="Jinja test")
+    return render_template("main.html", title="Jinja test")
 
 if __name__ == '__main__':
     app.run(debug=True)
