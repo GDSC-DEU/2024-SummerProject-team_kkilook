77114256a04326ab8ddba4f867bec748f6dd1c2b
MinGyeong Kim <mink0502k@gmail.com>
2024-08-02 21:55:50 +0900

    1

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
-app.config['DEBUG'] ='True'
-
+app = Flask(__name__, template_folder='Front-end/templates', static_folder='Front-end/static')
+app.config['DEBUG'] = True
 
 @app.route("/")
 def hello():
-    return render_template("main.html", title="Jinja test")
+    return render_template("main.html", title="Jinja test1")
+
+@app.route("/test")
+def home():
+    return "Hello, Flask~~~"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
