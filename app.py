77eb59bbc771199dd88cbe9a76a665a6b5e982a1
MinGyeong Kim <mink0502k@gmail.com>
2024-08-05 05:50:12 +0900

    1

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, template_folder='Front-end/templates', static_folder='Front-end/static')
+
+@app.route('/Front-end/static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(app.static_folder, filename)
+
 app.config['DEBUG'] = True
 
 @app.route("/")
-def hello():
+def main():
     return render_template("main.html")
 
+@app.route("/heart")
+def heart():
+    return render_template("heart.html")
+
+@app.route("/month")
+def month():
+    return render_template("month.html")
+
+@app.route("/medium")
+def medium():
+    return render_template("medium.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
