1472ba4504dd18cf662d5cef0c84f54f5c120f7a
MinGyeong Kim <mink0502k@gmail.com>
2024-08-05 01:09:56 +0900

    mk

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__, template_folder='Front-end/templates', static_folder='Front-end/static')
+
 app.config['DEBUG'] = True
 
 @app.route("/")
@@ -15,6 +16,10 @@ def heart():
 @app.route("/month")
 def month():
     return render_template('month.html')
+
+@app.route("/medium")
+def medium():
+    return render_template('medium.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
