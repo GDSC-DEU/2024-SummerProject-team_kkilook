2c00df67cf328bdb0486bf80cc56e5986e08f173
MinGyeong Kim <mink0502k@gmail.com>
2024-08-05 02:24:13 +0900

    11

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def hello():
-    return render_template("main.html", title="Jinja test1")
+    return render_template("main.html")
 
-@app.route("/test")
-def home():
-    return "Hello, Flask~~~"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
